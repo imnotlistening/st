54e0f0a05bd255296e490621aefd0a2e91b8c4d0
Alex Waterman <alexw@nvidia.com>
2019-02-06 12:37:44 -0800

    SQUASH: Organized the printing code a bit
    Still not perfect, by any stretch of the imagination, but it's making
    progress.
    
    Signed-off-by: Alex Waterman <alexw@nvidia.com>

--- pysrc/cmd_portfolio.py
+++ pysrc/cmd_portfolio.py
@@ -4,6 +4,7 @@
 
 import sys
 import argparse
+import math
 
 from StringIO import StringIO
 
@@ -15,9 +16,15 @@ from stcommand import st_add_alias
 from stock.portfolio import Portfolio
 from stock.lot       import LotAggregate
 
-portfolio_header = '%-5s %9s %10s | %-6s %11s %11s %10s %10s'
-portfolio_lines  = '%-5s %9s %10s + %-6s %11s %11s %10s %10s'
-portfolio_fields = '%-5s %9.2f %10s | %-6d %11.2f %11.2f %10.2f %10.2f'
+# Regular, 80 char wide print
+portfolio_header = '%-5s %9s %17s | %-6s %11s %10s %10s'
+portfolio_lines  = '%-5s %9s %17s + %-6s %11s %10s %10s'
+portfolio_fields = '%-5s %9.2f %17s | %-6d %11.2f %10.2f %10.2f'
+
+# Expanded line - this adds the cost basis to the print.
+portfolio_header_ex = '%-5s %9s %17s | %-6s %11s %11s %10s %10s'
+portfolio_lines_ex  = '%-5s %9s %17s + %-6s %11s %11s %10s %10s'
+portfolio_fields_ex = '%-5s %9.2f %17s | %-6d %11.2f %11.2f %10.2f %10.2f'
 
 # Let's us handle errors instead of argparse printing a useless help message
 # and killing our app.
@@ -128,6 +135,107 @@ class STCommandPortfolio(STCommand):
 
         return STCommand.SUCCESS
 
+    def print_lot_details(self, s):
+        """
+        Print the details for all lots relevant to the passed stock.
+        """
+
+        self.println('  # %s' % s.name())
+
+        for l in self.portfolio.asset_lots[s]:
+            change = s.price() - l.acquire_price
+            change_pc = (change / l.acquire_price) * 100
+
+            self.println('  %s: %5d @ %7.02f$  %8.02f %7.02f%%' % (
+                l.date.strftime('%b %d, %Y'),
+                l.nr, l.acquire_price,
+                change, change_pc))
+
+        self.println('')
+
+    def print_normal(self):
+        """
+        This prints the output assuming a 80 char wide terminal. That limits how
+        much we can display in a line.
+        """
+
+        self.println(portfolio_header % (
+            'Symb',
+            'Price ($)',
+            'Change',
+            'Shares',
+            'Value',
+            'Change',
+            'Gain'))
+
+        self.println(portfolio_lines % (
+            '----',
+            '---------',
+            '------',
+            '------',
+            '-----',
+            '------',
+            '----'    ))
+
+        total_equity = 0.0
+        daily_change = 0.0
+
+        for s in sorted(self.portfolio.assets):
+
+            la = self.portfolio.sum_stock(s)
+
+            total_equity += la.shares * s.price()
+            daily_change += la.shares * s.change()
+
+            c = None
+            direction = ''
+            if s.change() > 0:
+                direction = u'\u25b2'
+                c = STLine.GREEN
+            elif s.change() < 0:
+                direction = u'\u25bc'
+                c = STLine.RED
+
+            ca, cp = s.get_change()
+            ca = math.fabs(ca)
+            cp = math.fabs(cp * 100)
+
+            self.println(
+                portfolio_fields % (
+                    s.symb(),
+                    s.price(),
+                    '%6.02f %5.02f%% %1s' % (ca, cp, direction),
+                    la.shares,
+                    la.shares * s.price(),
+                    la.change,
+                    la.gain),
+                color=c)
+
+            if self.args.details:
+                self.print_lot_details(s)
+
+        self.println('')
+        return total_equity, daily_change
+
+    def print_expanded(self):
+        return
+
+    def print_summary(self, total_equity, daily_change):
+        """
+        Print a summary for the portfolio.
+        """
+
+        total_cash      = 0.0
+        portfolio_value = 0.0
+
+
+        self.println('Portfolio Statistics:')
+        self.println('  Total equity: %10.2f' % total_equity)
+        self.println('  Total cash:   %10.2f' % self.portfolio.cash)
+        self.println('  Total value:  %10.2f' % (total_equity +
+                                                 self.portfolio.cash))
+        self.println('  Daily change: %10.2f' % daily_change)
+
     def update(self, cmdlist):
         """
         Assumes the update_init() was successfull which means the arg parsing
@@ -150,81 +258,8 @@ class STCommandPortfolio(STCommand):
             self.portfolio.refresh()
         self.runs += 1
 
-        # Now start printing stuff.
-        self.println(portfolio_header % (
-            'Symb',
-            'Price ($)',
-            'Change',
-            'Shares',
-            'Cost Basis',
-            'Value',
-            'Change',
-            'Gain'))
-
-        self.println(portfolio_lines % (
-            '----',
-            '---------',
-            '------',
-            '------',
-            '---------',
-            '-----',
-            '------',
-            '----'    ))
-
-        total_equity    = 0.0
-        total_cash      = 0.0
-        portfolio_value = 0.0
-        daily_change    = 0.0
-
-        for s in sorted(self.portfolio.assets):
-
-            la = self.portfolio.sum_stock(s)
-
-            c = None
-            direction = ''
-            if s.change() > 0:
-                direction = u'\u25b2'
-                c = STLine.GREEN
-            elif s.change() < 0:
-                direction = u'\u25bc'
-                c = STLine.RED
-
-            self.println(
-                portfolio_fields % (
-                    s.symb(),
-                    s.price(),
-                    '%-1s %6.02f' % (direction, s.change()),
-                    la.shares,
-                    la.cb,
-                    la.shares * s.price(),
-                    la.change,
-                    la.gain),
-                color=c)
-
-            total_equity += la.shares * s.price()
-            daily_change += la.shares * s.change()
-
-            if not self.args.details:
-                continue
-
-            for l in lot_dict[symb]:
-                change = l.stock.price() - l.acquire_price
-                change_pc = (change / l.acquire_price) * 100
-
-                self.println('  %s: %5d @ %7.02f  $%8.02f [%7.02f%%]' % (
-                    l.date.strftime('%b %d, %Y'),
-                    l.nr, l.acquire_price,
-                    change, change_pc))
-
-            self.println('')
-
-        self.println('')
-        self.println('Portfolio Statistics:')
-        self.println('  Total equity: %10.2f' % total_equity)
-        self.println('  Total cash:   %10.2f' % self.portfolio.cash)
-        self.println('  Total value:  %10.2f' % (total_equity +
-                                                 self.portfolio.cash))
-        self.println('  Daily change: %10.2f' % daily_change)
+        total_equity, daily_change = self.print_normal()
+        self.print_summary(total_equity, daily_change)
 
         return STCommand.SUCCESS
 
